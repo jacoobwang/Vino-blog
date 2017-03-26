e5570b7526416d4e1d5f92d811ca4b26808a5e74
jacoobwang <531532957@qq.com>
2017-03-26 16:09:03 +0800

    fix composer install check

--- index_console.php
+++ index_console.php
@@ -7,8 +7,17 @@
  */
 namespace console;
 
-require __DIR__ . '/vendor/autoload.php';
+if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require __DIR__ . '/vendor/autoload.php';
 
-$app = new \console\Application(__DIR__);
-$app->handleRequest();
+    $app = new \console\Application(__DIR__);
+    $app->handleRequest();
+} else {
+    echo "please install composer first,visit this page see how to install it http://docs.phpcomposer.com/00-intro.html \n";
+    exit;
+}
+
+
+
+
 
